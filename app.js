49d7bb0873873e542405d0902c5432416614e4da
Vipul Dang <vipuldang1998@gmail.com>
2020-07-07 20:31:28 +0530

    hi

--- app.js
+++ app.js
@@ -80,6 +80,6 @@ app.use('/admin/comments',comments);
 
 
 var port = process.env.PORT | 3000;
-app.listen(port,()=>{
+app.listen(port,'0.0.0.0',()=>{
   console.log(`listening to port `+port);
 });
